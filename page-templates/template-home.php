7a6399520fa216c0f809854c0bed5ee23a218bdb
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-06-08 00:50:20 +0200

    added hero

---
+++ page-templates/template-home.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template Name: Template Home
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+
+if ( is_front_page() ) {
+	get_template_part( 'global-templates/hero' );
+}
+?>
+
+
+<div class="home-hero">
+    <div class="hero-circle"></div>
+    <div class="container">
+        <div class="row hero-content">
+            <div class="col-lg-6">
+                <p class="text-hero">Miło mi powitać Cię w </br>klinice dentystycznej Biały Ząbek</p>
+                <h1 class="text-hero-title">Przyjazna atmosfera oraz poczucie bezpieczeństwa</h1>
+                <p class="text-hero-description">Oddaj się pod fachową opiekę najlepszych specjalistów. Nasi stomatolodzy dołążą wszelkich starań, aby pomóc Ci nawet w najcięższych przypadkach</p>
+                <button type="button" class="btn">Przekonaj się - umów wizytę</button>
+            </div>
+            <div class="col-lg-6">
+                <img src="<?php echo get_template_directory_uri(); ?>/src/img/pacjent-hero.png" alt="Pacjent u dentysty"/>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
