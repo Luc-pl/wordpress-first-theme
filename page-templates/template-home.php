e5c3937d3dd8d6165eb87cda6f39a6f91b66ea56
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-06-09 00:03:39 +0200

    added section services-two

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -108,7 +108,63 @@ if ( is_front_page() ) {
     </div>
 </section>
 
-
+<section class="services-two">
+    <div class="container">
+        <h3 class="title-services">Our Umbrella of Services</h3>
+        <p class="subtitle-services">Pain free procedures in dentistry</p>
+        <div class="row">
+            <div class="col-lg-4 category-services px-5">
+                <h5 class="category-title">Teeth Whitening</h5>
+                <p class="category-description">Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                <button class="btn-services">Sprawdź <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
+                </button>
+            </div>
+            <div class="col-lg-4 category-services px-5">
+                <h5 class="category-title">Teeth Cleaning</h5>
+                <p class="category-description">Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                <button class="btn-services">Sprawdź <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
+                </button>
+            </div>
+            <div class="col-lg-4 category-services px-5">
+                <h5 class="category-title">Modern Anesthetic</h5>
+                <p class="category-description">Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                <button class="btn-services">Sprawdź <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
+                </button>
+            </div>
+            <div class="col-lg-4 category-services px-5">
+                <h5 class="category-title">Quality Brackets</h5>
+                <p class="category-description">Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                <button class="btn-services">Sprawdź <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
+                </button>
+            </div>
+            <div class="col-lg-4 category-services px-5">
+                <h5 class="category-title">Teeth Whitening</h5>
+                <p class="category-description">Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                <button class="btn-services">Sprawdź <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
+                </button>
+            </div>
+            <div class="col-lg-4 category-services px-5">
+                <h5 class="category-title">Teeth Whitening</h5>
+                <p class="category-description">Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
+                <button class="btn-services">Sprawdź <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="btn-all">
+                <button class="btn-services-all">Sprawdź wszystkie <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
+                </button>
+        </div>
+        </div>
+    </div>
+</section>
 
 <?php
 get_footer();
