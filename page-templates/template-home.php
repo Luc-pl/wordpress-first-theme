5905cff3c826ded32ea9f3a447c4c5f6b8bcbacf
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-06-08 17:04:25 +0200

    added sections: hero & cta

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -18,23 +18,32 @@ if ( is_front_page() ) {
 }
 ?>
 
-
-<div class="home-hero">
-    <div class="hero-circle"></div>
-    <div class="container">
-        <div class="row hero-content">
-            <div class="col-lg-6">
-                <p class="text-hero">Miło mi powitać Cię w </br>klinice dentystycznej Biały Ząbek</p>
-                <h1 class="text-hero-title">Przyjazna atmosfera oraz poczucie bezpieczeństwa</h1>
-                <p class="text-hero-description">Oddaj się pod fachową opiekę najlepszych specjalistów. Nasi stomatolodzy dołążą wszelkich starań, aby pomóc Ci nawet w najcięższych przypadkach</p>
-                <button type="button" class="btn">Przekonaj się - umów wizytę</button>
-            </div>
-            <div class="col-lg-6">
-                <img src="<?php echo get_template_directory_uri(); ?>/src/img/pacjent-hero.png" alt="Pacjent u dentysty"/>
+<section class="hero">
+    <div class="home-hero">
+        <div class="hero-circle"></div>
+        <div class="container">
+            <div class="row hero-content">
+                <div class="col-lg-6">
+                    <p class="text-hero">Miło mi powitać Cię w </br>klinice dentystycznej Biały Ząbek</p>
+                    <h1 class="text-hero-title">Przyjazna atmosfera oraz poczucie bezpieczeństwa</h1>
+                    <p class="text-hero-description">Oddaj się pod fachową opiekę najlepszych specjalistów. Nasi stomatolodzy dołążą wszelkich starań, aby pomóc Ci nawet w najcięższych przypadkach</p>
+                    <button type="button" class="btn">Umów termin wizyty</button>
+                </div>
+                <div class="col-lg-6">
+                    <img src="<?php echo get_template_directory_uri(); ?>/src/img/pacjent-hero.png" alt="Pacjent u dentysty"/>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</section>
+
+<section class="cta">
+    <div class="container">
+        <div class="row">
+            <p>Potrzebujesz porady? Zadzwoń: </br><span><a href="tel:(32) 123 45 67">(32) 123 45 67</a></span></p>
+        </div>
+    </div>
+</section>
 
 <?php
 get_footer();
